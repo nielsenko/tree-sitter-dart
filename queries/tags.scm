6aba207ee79f1d302274b8c413692c5f3ca5b2e1
Kasper Overgård Nielsen <kasper@byolimit.com>
2026-03-09 13:10:47 +0100

    feat: add highlights and tags queries
    - Add highlights.scm with complete syntax highlighting queries
    - Add tags.scm with symbol tagging queries
    - Fix additive operator tokenization (remove token() wrapper)
    - Use prec.left for equality/relational expressions
    - Remove 4 unnecessary conflicts
    - Update tree-sitter.json with highlights and tags paths

---
+++ queries/tags.scm
@@ -0,0 +1,67 @@
+; tags.scm - Dart symbol tagging queries
+
+; Classes
+(class_definition
+  name: (identifier) @name) @definition.class
+
+; Mixins
+(mixin_declaration
+  (identifier) @name) @definition.class
+
+; Extensions
+(extension_declaration
+  name: (identifier) @name) @definition.class
+
+; Extension types
+(extension_type_declaration
+  name: (extension_type_name
+    (identifier) @name)) @definition.class
+
+; Enums
+(enum_declaration
+  name: (identifier) @name) @definition.class
+
+; Top-level functions
+(source_file
+  (function_signature
+    name: (identifier) @name)
+  (function_body)) @definition.function
+
+; Top-level getters
+(source_file
+  (getter_signature
+    name: (identifier) @name)
+  (function_body)) @definition.function
+
+; Top-level setters
+(source_file
+  (setter_signature
+    name: (identifier) @name)
+  (function_body)) @definition.function
+
+; Methods
+(method_signature
+  (function_signature
+    name: (identifier) @name)) @definition.method
+
+; Getters (as methods)
+(method_signature
+  (getter_signature
+    name: (identifier) @name)) @definition.method
+
+; Setters (as methods)
+(method_signature
+  (setter_signature
+    name: (identifier) @name)) @definition.method
+
+; Constructors
+(constructor_signature
+  name: (identifier) @name) @definition.method
+
+; Typedefs
+(type_alias
+  (type_identifier) @name) @definition.type
+
+; Enum constants
+(enum_constant
+  name: (identifier) @name) @definition.constant
